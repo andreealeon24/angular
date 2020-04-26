281892f833b58b37d0e53a194e09c36ac122fe4a
Andreea <leon.andreea05@gmail.com>
2020-04-26 17:30:33 +0300

    commit

--- MedicalProject/MedicalProject/src/app/app-routing.module.ts
+++ MedicalProject/MedicalProject/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {MainNavComponent} from './main-nav/main-nav.component';
 
 
 const routes: Routes = [
-  {path:'', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'home', component:MainNavComponent}
 ];
 
 @NgModule({
--- MedicalProject/MedicalProject/src/app/app.module.ts
+++ MedicalProject/MedicalProject/src/app/app.module.ts
@@ -3,23 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LoginComponent } from './login/login.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainNavComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +38,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatToolbarModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
